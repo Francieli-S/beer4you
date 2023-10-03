6d052d8611b4c6317c76390f43da698ccbab1dcd
francieli.sauthier <francieli.sauthier@gmail.com>
2023-10-03 14:21:53 +0200

    get beers and install axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import axios from 'axios'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  const apiBeer = 'https://ih-beers-api2.herokuapp.com/beers'
+
+  const data = async () => {
+    try {
+      const response = await axios.get(apiBeer)      
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    data()
+  }, [])
 
   return (
     <>
