2ef001517ab61abe7d73baa09fcf6fc605aa0571
francieli.sauthier <francieli.sauthier@gmail.com>
2023-10-20 17:24:19 +0200

    feat: create footer and import to homepage
    Co-authored-by: Gabriela <knop.gm@gmail.com>

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import Container from '../container/Container';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <Container>
+      <div className='wrapper'>
+        <div>Footer</div>
+        <p>© Copyright Beer4you 2023</p>
+        </div>
+      </Container>
+    </div>
+  );
+}
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,14 @@
 import BeersGrid from '../../components/beers/BeersGrid';
 import Container from '../../components/container/Container';
+import Footer from '../../components/footer/Footer';
 
 export default function HomePage() {
   return (
-    <Container>
-      <BeersGrid />
-    </Container>
+    <>
+      <Container>
+        <BeersGrid />
+      </Container>
+      <Footer />
+    </>
   );
 }
