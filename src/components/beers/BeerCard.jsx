48b5f65667dd96dfec0161ebe0b5dd0bf7408a8d
knopgm <knop.gm@gmail.com>
2023-10-24 17:42:54 +0200

    refactor: add props
    Co-authored-by: Francieli <francieli.sauthier@gmail.com>

--- src/components/beers/BeerCard.jsx
+++ src/components/beers/BeerCard.jsx
@@ -1,12 +1,28 @@
-export default function BeerCard({ name, image }) {
-
+export default function BeerCard({
+  name,
+  image,
+  subtitle,
+  firstBrewed,
+  description,
+  alcoholLevel,
+  brewersTips,
+  brewmaster,
+  today,
+}) {
   return (
     <>
       <div>
-        <h3>{name}</h3>
         <div>
-          <img className='beers-img' src={image} />
+          <img className="beers-img" src={image} />
         </div>
+        <h3>{name}</h3>
+        <h4>{subtitle}</h4>
+        <p>{firstBrewed}</p>
+        <p>{description}</p>
+        <p>{alcoholLevel}</p>
+        <p>{brewersTips}</p>
+        <p>{brewmaster}</p>
+        <p>{today}</p>
       </div>
     </>
   );
