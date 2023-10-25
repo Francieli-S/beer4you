bec95c7587b0742690668f09833457c91a6869a0
knopgm <knop.gm@gmail.com>
2023-10-25 16:20:18 +0200

    feat: add router
    Co-authored-by: Francieli <francieli.sauthier@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homePage/HomePage';
+import BeerCartPage from './pages/beerCartPage/BeerCartPage';
 
 function App() {
   return (
-    <>
-     <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/beers/:beer_id" element={<BeerCartPage />} />
+    </Routes>
   );
 }
 
