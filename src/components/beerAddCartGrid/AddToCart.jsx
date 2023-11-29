adfbce3fa9981997afb6934e630c4d3bbf9673c7
francieli.sauthier <francieli.sauthier@gmail.com>
2023-11-29 15:52:32 +0100

    feat: create user interactions to update total
    Co-authored-by: Gabriela <knop.gm@gmail.com>

--- src/components/beerAddCartGrid/AddToCart.jsx
+++ src/components/beerAddCartGrid/AddToCart.jsx
@@ -1,20 +1,44 @@
+import { useState } from 'react';
 import './AddToCart.css';
 
 export default function AddToCart({ name }) {
+  const beerPrice = 2.49;
+
+  const [quantity, setQuantity] = useState(1);
+  const [total, setTotal] = useState(beerPrice);
+
+  const calcTotal = (quant, price) => {
+    return (quant * price).toFixed(2) * 1;
+  };
+
+  const handleClickAdd = () => {
+    const result = quantity + 1;
+    setQuantity(result);
+    setTotal(calcTotal(result, beerPrice));
+  };
+
+  const handleClickSubstract = () => {
+    if (quantity > 1) {
+      const result = quantity - 1;
+      setQuantity(result);
+      setTotal(calcTotal(result, beerPrice));
+    }
+  };
+
   return (
     <div className='add-to-cart'>
       <h2>{name}</h2>
       <div className='add-to-cart_infos'>
         <p>330ml</p>
-        <p>$ 2,49</p>
+        <p>$ {beerPrice}</p>
       </div>
       <div className='add-to-cart_infos'>
         <div className='add-to-cart_infos'>
-          <button>-</button>
-          <p>1x</p>
-          <button>+</button>
+          <button onClick={handleClickSubstract}>-</button>
+          <p>{quantity} x</p>
+          <button onClick={handleClickAdd}>+</button>
         </div>
-        <p>total</p>
+        <p> $ {total}</p>
       </div>
       <button>Add to Cart</button>
     </div>
